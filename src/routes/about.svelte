aa9ce3b1899e941ab4802a498b67186f80dcf969
Mihai Gabreanu <mihai.gabreanu@gmail.com>
2021-04-16 04:57:33 +0100

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<style lang='scss'>
+	@import './style/theme.scss';
+
+	h1 {
+		color: $main-color;
+	}
+</style>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
